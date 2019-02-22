653cb7947a1de47e0131e13961c72e3df6bd7a7f
kraken9x <tranminhcuong95@gmail.com>
2019-02-22 22:07:57 +0700

    second commit

---
+++ Session1/intro.py
@@ -0,0 +1,21 @@
+# n = "Nam" #Cách khai báo kiểu dữ liệu string
+# age = 22 #Cách khai báo kiểu dữ liệu number
+
+# string = f"I'm {age} years {age} old "
+# print("Hello my friend", n)
+# print(string)
+# print("I'm", age, "years old")
+# dấu "=" có ý nghĩa là assign GÁN
+# n = 4
+# n = 5
+# print(n)
+
+# Những dòng code trên gọi là hard code. Code theo ý mình, không có sự tương tác với người dùng
+# Hàm input() Dùng để người dùng tương tác, nhập thông tin mong muốn vào và tương tác với code
+# Hàm input() Mặc định trả về kết quả là string
+n = input("Nhập năm sinh: ")
+m = 2019-int(n)
+print("Tuổi của bạn là",m)
+# Trong lập trình quan trọng hiểu đc làm ra ứng dụng này làm gì
+
+
---
+++ Session1/turtle_intro.py
@@ -0,0 +1,41 @@
+from turtle import *
+
+# forward(300)
+# left(90)
+# forward(300)
+# left(90)
+# forward(300)
+# left(90)
+# forward(300)
+# left(150)
+# forward(300)
+# right(90)
+# forward(300)
+# right(90)
+# forward(300)
+# right(90)
+# forward(300)
+# right(120)
+# forward(300)
+# right(90)
+# forward(300)
+# right(90)
+# forward(300)
+# right(90)
+# forward(300)
+
+#code ở trên lặp lại quá nhiều
+# => for i in range():
+# code ở trong muốn lặp phải thụt vào
+speed(0)
+for k in range(100):
+    for i in range(4):
+        forward(300)
+        left(90)
+    left(2)    
+
+
+
+
+
+mainloop()
